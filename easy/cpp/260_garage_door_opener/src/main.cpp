d009d6936fcbf56350276e3deed13147d5036ca2
crr0004 <crr0004@gmail.com>
2016-04-03 11:17:07 +1000

    Created states and transitions needed for challenge. Still got to figure out how I am going to move from state to state, where that happens and how the user inputs button presses

--- easy/cpp/260_garage_door_opener/src/main.cpp
+++ easy/cpp/260_garage_door_opener/src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct state {
+	string name;
+};
+
+struct transition {
+	state* next;
+} ;
+
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;	
+	state* current_state;
+
+	state open;
+	state closed;
+	state closing;
+	state opening;
+	state opening_interrupted;
+	state closing_interrupted;
+
+	transition open_closing;
+	transition closed_opening;
+	transition opening_open;
+	transition closing_closed;
+	transition closing_closing_interrupted;
+	transition opening_opening_interrupted;
+	transition closing_interrupted_opening;
+	transition opening_interrupted_closing;
+
+
+
 	return 0;
 }
