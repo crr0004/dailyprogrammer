6701a6d9942cac3c58099e6d9566230a64cb6e4b
crr0004 <crr0004@gmail.com>
2016-04-08 19:00:03 +1000

    Basic transitions now. Using internal transition control with a virtual base class. Context is now just an integer linked to an array. Probably need to clean up pointer usage, such as in constructors(?).

--- easy/cpp/260_garage_door_opener/src/main.cpp
+++ easy/cpp/260_garage_door_opener/src/main.cpp
@@ -3,34 +3,65 @@
 
 using namespace std;
 
-struct state {
-	string name;
+class State {
+	private:
+		string name;
+	public:
+		State(){}
+		~State() {}
+		State(string name): name(name){}
+		virtual void transit(int *current_state){}
+		string get_name(){
+			return name;
+		}
 };
 
-struct transition {
-	state* next;
-} ;
+class Open_State: public State {
+	public:
+		void transit(int *current_state){
+
+			cout << current_state << endl;
+			if((*current_state) == 0){
+				(*current_state) = 1;
+			}
+
+
+		}
+	Open_State(string name): State(name){}
+
+};
+class Closing_State: public State {
+	public:
+		void transit(int *current_state){
+
+
+		}
+	Closing_State(string name): State(name){}
+
+};
 
 int main(int argc, char *argv[])
 {
-	state* current_state;
 
-	state open;
-	state closed;
-	state closing;
-	state opening;
-	state opening_interrupted;
-	state closing_interrupted;
+	State *states[6] = { };
+	int current_state = 0;
+	Open_State open("Open");
+	Closing_State closing("Closing");
 
-	transition open_closing;
-	transition closed_opening;
-	transition opening_open;
-	transition closing_closed;
-	transition closing_closing_interrupted;
-	transition opening_opening_interrupted;
-	transition closing_interrupted_opening;
-	transition opening_interrupted_closing;
+	states[0] = &open;
+	states[1] = &closing;
 
+	string input;
+
+	cout << "Current state: " << states[current_state]->get_name() << endl;
+
+	while(input != "end"){
+		getline(cin,input);
+		states[current_state]->transit(&current_state);
+
+		cout << "Current state: " << states[current_state]->get_name() << endl;
+
+	}
 
 
 	return 0;
